5ea04ee4f5824cf373d564d264ad45f97654d4f1
ProGitMounir <pro.mailmounir@gmail.com>
2025-05-28 10:30:15 +0100

    finalisation acces client via le serveur

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -3,8 +3,13 @@ import { HelloRouteur } from "./routes/hello.router";
 const app = express();
 const port = 3000;
 
+const path = require("path");
+const DIST_DIR = path.join(__dirname, "../../../Client/dist");
+const HTML_FILE = path.join(DIST_DIR, "index.html");
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
 
-app.use("/hello", HelloRouteur);
+//app.use("/hello", HelloRouteur);
+app.use(express.static(DIST_DIR));
